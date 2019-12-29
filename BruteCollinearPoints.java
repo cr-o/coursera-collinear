7b8c2a01535521b8abd184bafb052e5952219c69
catbeans <workemailzoop@gmail.com>
2019-12-29 03:05:44 -0500

    brute collinear setup

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -5,7 +5,18 @@
  **************************************************************************** */
 
 public class BruteCollinearPoints {
-    public static void main(String[] args) {
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
 
     }
 }
